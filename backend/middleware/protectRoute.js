cc4a32795abe5a7eaf7d9a0d402f156c7d41e6ce
DanishSofii <bashirdanish85@gmail.com>
2024-03-30 16:28:29 +0530

    jwt env enable

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -10,7 +10,7 @@ const protectRoute = async(req,res,next)=>{
         if(!token){
             return res.status(401).json({error:"Unauthorized : No token provided"})
         }
-        const decoded = jwt.verify(token,JWT_SECRET);
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
 
         if(!decoded){
             return res.status(401).json({error:"Unauthorized : Invalid Token"});
