ad6d09ff05e4058fe3f0b57ba80ad04162b892ae
DanishSofii <bashirdanish85@gmail.com>
2024-03-30 20:06:55 +0530

    render link socket io update for cors error

--- frontend/src/hooks/useGetConversations.js
+++ frontend/src/hooks/useGetConversations.js
@@ -2,29 +2,32 @@ import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 const useGetConversations = () => {
-	const [loading, setLoading] = useState(false);
-	const [conversations, setConversations] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [conversations, setConversations] = useState([]);
 
-	useEffect(() => {
-		const getConversations = async () => {
-			setLoading(true);
-			try {
-				const res = await fetch("/api/users");
-				const data = await res.json();
-				if (data.error) {
-					throw new Error(data.error);
-				}
-				setConversations(data);
-			} catch (error) {
-				toast.error("get conversation error",error.message);
-			} finally {
-				setLoading(false);
-			}
-		};
+    useEffect(() => {
+        const getConversations = async () => {
+            setLoading(true);
+            try {
+                const res = await fetch("/api/users");
+                if (!res.ok) {
+                    throw new Error(`Failed to fetch conversations: ${res.statusText}`);
+                }
+                const data = await res.json();
+                if (data.error) {
+                    throw new Error(data.error);
+                }
+                setConversations(data);
+            } catch (error) {
+                toast.error(`Error while fetching conversations: ${error.message}`);
+            } finally {
+                setLoading(false);
+            }
+        };
 
-		getConversations();
-	}, []);
+        getConversations();
+    }, []);
 
-	return { loading, conversations };
+    return { loading, conversations };
 };
 export default useGetConversations;
