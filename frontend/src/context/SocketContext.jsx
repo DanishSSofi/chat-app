21ad48a803dd91d6cbd402a2b7ec26bd0f496b97
DanishSofii <bashirdanish85@gmail.com>
2024-03-30 19:31:39 +0530

    render link socket io update for cors error

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({children})=>{
 
     useEffect(()=>{
         if(authUser){
-            const socket = io("http://localhost:5000",{
+            const socket = io("https://connectus-b6mr.onrender.com/",{
                 query:{
                     userId:authUser._id,
                 }
